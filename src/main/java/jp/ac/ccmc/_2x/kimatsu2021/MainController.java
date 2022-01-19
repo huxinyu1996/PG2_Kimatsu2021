3e7c6ddf50cb7a1bf55446bb00012bf7d613772a
huxinyu1996 <84057422+huxinyu1996@users.noreply.github.com>
2022-01-19 12:19:01 +0900

    Update MainController.java

--- src/main/java/jp/ac/ccmc/_2x/kimatsu2021/MainController.java
+++ src/main/java/jp/ac/ccmc/_2x/kimatsu2021/MainController.java
@@ -56,13 +56,17 @@ public class MainController {
 
 	@GetMapping("/delete/{id}")
 	public String confirmDeleting(@PathVariable(name="id") int id, Model model) {
-		service.delete(id);
-        return "redirect:/";
+		Account account = service.get(id);
+		model.addAttribute("account", account);
+        return "delete";
 	}
 
 	@PostMapping("/delete")
 	public String deleteAccount(@ModelAttribute("account") Account account, RedirectAttributes redirectAttributes) {
 		service.delete(account.getId());
+		String message = "#" + "「" + account.getName() + "」を削除しました。";
+		redirectAttributes.addFlashAttribute("message", message);
+		
 		return "redirect:/";
 	}
 
